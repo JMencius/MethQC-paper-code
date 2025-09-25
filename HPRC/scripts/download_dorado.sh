65127ec05735e777c921ac13084d45ec9f161602
Jun Mencius <63288733+JMencius@users.noreply.github.com>
2025-09-25 15:34:00 +0800

    Update download_dorado.sh
    Correct dorado version

--- HPRC/scripts/download_dorado.sh
+++ HPRC/scripts/download_dorado.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://cdn.oxfordnanoportal.com/software/analysis/dorado-0.9.5-linux-x64.tar.gz;
+wget https://cdn.oxfordnanoportal.com/software/analysis/dorado-0.7.2-linux-x64.tar.gz;
 
-tar -zxvf dorado-0.9.5-linux-x64.tar.gz;
+tar -zxvf dorado-0.7.2-linux-x64.tar.gz;
 
-./dorado-0.9.5-linux-x64/bin/dorado --version;
+./dorado-0.7.2-linux-x64/bin/dorado --version;
