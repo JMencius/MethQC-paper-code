36f56da40fe8d66a0bd7b157f68ce14bc7b12d0d
Jun Mencius <63288733+JMencius@users.noreply.github.com>
2025-11-10 16:15:03 +0800

    Update samtools.sh

--- ONT/scripts/conda_envs/samtools.sh
+++ ONT/scripts/conda_envs/samtools.sh
@@ -5,4 +5,6 @@ conda create -n samtools;
 conda_base=$(conda info --base);
 source "$conda_base"/etc/profile.d/conda.sh;
 
-conda install -c bioconda samtools>=1.6;
+conda install -c bioconda samtools=1.6
+
+samtools --version;
